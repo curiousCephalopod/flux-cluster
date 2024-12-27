b1381a030bd9e3188412fd766133fa435fc8e3b1
curiousCephalopod <me@curiouscephalopod.com>
2024-12-27 16:49:54 +0000

    updates

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationDestination,ReplicationSource"
+    "ExternalSecret,Secret,ReplicationDestination,ReplicationSource"
     "-schema-location"
     "default"
     "-schema-location"
